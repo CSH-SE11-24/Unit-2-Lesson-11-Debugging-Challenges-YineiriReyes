d435489ca3eccc53891d6526b8e561307bdc9122
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 17:13:24 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Challenge 1: Fix the syntax error
+console.log("Hello, World")
+
+// Challenge 2: Fix the reference error
+ let num = 10;
+ console.log(`num is ${num}`);
+
+// Challenge 3: Fix the issue with this if statement
+ let isRainy = true
+if (isRainy !==true) {
+  console.log("No umbrella needed!")
+ }
+
+// Challenge 4: Fix the issue with this if statement
+let age = 16;
+if (age >= 18){
+  console.log("You can vote!");
+}else {
+   console.log("You can't vote.");
+ }
+
+
+// Challenge 5: Find and fix the issue with this while loop
+let counter = 0
+while (counter <= 5) {
+ console.log(counter);
+ counter++
+}
+
+// Challenge 6: Find and fix the issue with this while loop
+let count = 1;
+ while (count < 5) {
+ console.log(count);
+  count++;
+ }
